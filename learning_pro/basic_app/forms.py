c67847fb24b49e97e673f5d4c68cdb8a10685836
Pradyumn Sharda <Pradyumn@Pradyumns-MacBook-Air.local>
2020-05-23 13:03:49 +0530

    first commit

---
+++ learning_pro/basic_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from basic_app.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')
